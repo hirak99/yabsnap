1cea0240f8d108665542a69ac7f98b5bc4cf8268
Nomen Aliud <hirak99@gmail.com>
2022-10-06 08:36:09 -0700

    install.sh to exclude test and other files

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ readonly MY_PATH=$(cd $(dirname "$0") && pwd)
 
 cd $MY_PATH
 
-rsync -aAXHSv src/ /usr/share/yabsnap --delete
+rsync -aAXHSv src/ /usr/share/yabsnap \
+  --exclude '*_test.py' \
+  --include '*/' --include '*.py' --include '*.sh' \
+  --exclude '*' \
+  --prune-empty-dirs \
+  --delete --delete-excluded
 ln -sf /usr/share/yabsnap/yabsnap.sh /usr/bin/yabsnap
 
 cp artifacts/services/yabsnap.service /etc/systemd/system
