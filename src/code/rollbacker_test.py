de45f293c6c89fd3d6aa8e48842d1f873221fa28
Nomen Aliud <hirak99@gmail.com>
2022-10-14 09:44:43 -0700

    Reorganized rollbacker_test mocks

--- src/code/rollbacker_test.py
+++ src/code/rollbacker_test.py
@@ -21,10 +21,30 @@ from . import snap_holder
 # For testing, we can access private methods.
 # pyright: reportPrivateUsage=false
 
+_MOUNT_LOOKUP = {
+    '/snaps': ('/dev/BLOCKDEV1', '/subv_snaps'),
+    '/home': ('/dev/BLOCKDEV1', '/subv_home'),
+    '/root': ('/dev/BLOCKDEV1', '/subv_root'),
+}
+
+
+def _mock_get_mount_attributes(mount_pt: str):
+  return rollbacker._MountAttributes(device=_MOUNT_LOOKUP[mount_pt][0],
+                                     subvol_name=_MOUNT_LOOKUP[mount_pt][1])
+
 
 class TestRollbacker(unittest.TestCase):
 
-  def test_checks(self):
+  def test_empty_snap(self):
+    generated = rollbacker._rollback_snapshots(to_rollback=[])
+    self.assertEqual(generated, ['# No snapshot matched to rollback.'])
+
+  @mock.patch.object(rollbacker, '_get_now_str', return_value='20220202220000')
+  @mock.patch.object(rollbacker,
+                     '_get_mount_attributes',
+                     side_effect=_mock_get_mount_attributes)
+  def test_two_snaps(self, mock_get_now_str: mock.Mock,
+                     mock_get_mount_attributes: mock.Mock):
     # config_list = [configs.Config('test.conf', source='/home', dest_prefix='/snaps/@home-')]
     snaps_list = [
         snap_holder.Snapshot('/snaps/@home-20220101130000'),
@@ -33,23 +53,7 @@ class TestRollbacker(unittest.TestCase):
     snaps_list[0].metadata.source = '/home'
     snaps_list[1].metadata.source = '/root'
 
-    mount_lookup = {
-        '/snaps': ('/dev/BLOCKDEV1', '/subv_snaps'),
-        '/home': ('/dev/BLOCKDEV1', '/subv_home'),
-        '/root': ('/dev/BLOCKDEV1', '/subv_root'),
-    }
-
-    def get_mock_mount(mount_pt: str):
-      return rollbacker._MountAttributes(device=mount_lookup[mount_pt][0],
-                                         subvol_name=mount_lookup[mount_pt][1])
-
-    with mock.patch.object(rollbacker,
-                           '_get_mount_attributes',
-                           side_effect=get_mock_mount):
-      with mock.patch.object(rollbacker,
-                             '_get_now_str',
-                             return_value='20220202220000'):
-        generated = rollbacker._rollback_snapshots(to_rollback=snaps_list)
+    generated = rollbacker._rollback_snapshots(to_rollback=snaps_list)
 
     expected = '''#!/bin/bash
 # Save this to a script, review and run as root to perform the rollback.
