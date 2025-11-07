b597a79c970f583dfdbb797851c640c00234a7b4
Arnab Bose <hirak99@gmail.com>
2025-11-07 20:42:24 +0530

    test(completions): add tests for shell implementations

--- src/code/autocomplete/shell_impl.py
+++ src/code/autocomplete/shell_impl.py
@@ -27,68 +27,87 @@ done
 """
 
 
+def _zsh_commands(
+    completions: list[comp_types.Completion],
+    file_completions: list[comp_types.FileCompletion],
+    messages: list[comp_types.Message],
+) -> str:
+    # NOTE:
+    # To view documentation on `compadd`, do this -
+    # ```sh
+    # unalias run-help
+    # autoload run-help
+    # run-help compadd
+    # ```
+
+    if file_completions:
+        return "_path_files"
+
+    # Returns lines defining two arres.
+    # They are meant to be `eval`-ed.
+    command_lines: list[str] = []
+    option_lines: list[str] = []
+    for x in completions:
+        entry = x.option
+        if x.help:
+            entry += f":{x.help}"
+        if x.type == comp_types.CompletionType.COMMAND:
+            command_lines.append(entry)
+        elif x.type == comp_types.CompletionType.OPTION:
+            option_lines.append(entry)
+        else:
+            logging.warning(f"Unknown completion type: {x.type}")
+
+    def describe(tag: str, lines: list[str]) -> list[str]:
+        result: list[str] = []
+        result += [f"local -a yabsnap_{tag}"]
+        result += [f"yabsnap_{tag}=("]
+        result += [f"  {shlex.quote(x)}" for x in lines]
+        result += [f")"]
+        result += [f"_describe -t {tag} 'yabsnap {tag}' yabsnap_{tag}"]
+        return result
+
+    sh_lines: list[str] = []
+    if command_lines:
+        sh_lines += describe("commands", command_lines)
+    if option_lines:
+        sh_lines += describe("options", option_lines)
+    if messages:
+        sh_lines += [f"compadd -x {shlex.quote(x.message)}" for x in messages]
+
+    return "\n".join(sh_lines)
+
+
+def _array_commands(
+    completions: list[comp_types.Completion],
+) -> str:
+    # Return array of words. Good for testing.
+    return " ".join(x.option for x in completions)
+
+
+def _bash_commands(
+    completions: list[comp_types.Completion],
+    file_completions: list[comp_types.FileCompletion],
+) -> str:
+    if file_completions:
+        return _BASH_COMPREPLY_FILES
+    words_str = " ".join(f"{shlex.quote(x.option)}" for x in completions)
+    return f"COMPREPLY=( {words_str} )"
+
+
 def shell_commands(
     completions: list[comp_types.Completion],
     file_completions: list[comp_types.FileCompletion],
     messages: list[comp_types.Message],
-):
+) -> str:
     style = os.environ.get("STYLE", "")
-    if style == "zsh":
-        # NOTE:
-        # To view documentation on `compadd`, do this -
-        # ```sh
-        # unalias run-help
-        # autoload run-help
-        # run-help compadd
-        # ```
-
-        if file_completions:
-            return "_path_files"
-
-        # Returns lines defining two arres.
-        # They are meant to be `eval`-ed.
-        command_lines: list[str] = []
-        option_lines: list[str] = []
-        for x in completions:
-            entry = x.option
-            if x.help:
-                entry += f":{x.help}"
-            if x.type == comp_types.CompletionType.COMMAND:
-                command_lines.append(entry)
-            elif x.type == comp_types.CompletionType.OPTION:
-                option_lines.append(entry)
-            else:
-                logging.warning(f"Unknown completion type: {x.type}")
-
-        def describe(tag: str, lines: list[str]) -> list[str]:
-            result: list[str] = []
-            result += [f"local -a yabsnap_{tag}"]
-            result += [f"yabsnap_{tag}=("]
-            result += [f"  {shlex.quote(x)}" for x in lines]
-            result += [f")"]
-            result += [f"_describe -t {tag} 'yabsnap {tag}' yabsnap_{tag}"]
-            return result
-
-        sh_lines: list[str] = []
-        if command_lines:
-            sh_lines += describe("commands", command_lines)
-        if option_lines:
-            sh_lines += describe("options", option_lines)
-        if messages:
-            sh_lines += [f"compadd -x {shlex.quote(x.message)}" for x in messages]
-
-        return "\n".join(sh_lines)
-
-    if style == "array":
-        # Return array of words. Good for testing.
-        return " ".join(x.option for x in completions)
-
-    if style == "bash":
-        # Return commands to be eval'ed in bash.
-        if file_completions:
-            return _BASH_COMPREPLY_FILES
-        words_str = " ".join(f"{shlex.quote(x.option)}" for x in completions)
-        return f"COMPREPLY=( {words_str} )"
-
-    logging.warning(f"STYLE not provided or is unknown: {style=}")
-    return ""
+    match style:
+        case "zsh":
+            return _zsh_commands(completions, file_completions, messages)
+        case "array":
+            return _array_commands(completions)
+        case "bash":
+            return _bash_commands(completions, file_completions)
+        case _:
+            logging.warning(f"STYLE not provided or is unknown: {style=}")
+            return ""
---
+++ src/code/autocomplete/shell_impl_test.py
@@ -0,0 +1,88 @@
+import unittest
+
+from . import comp_types
+from . import shell_impl
+
+# For testing, we can access private methods.
+# pyright: reportPrivateUsage=false
+
+_COMPLETIONS: list[comp_types.Completion] = [
+    comp_types.Completion(
+        option="foo", help="foo help", type=comp_types.CompletionType.COMMAND
+    ),
+    comp_types.Completion(
+        option="--bar", help="bar help", type=comp_types.CompletionType.OPTION
+    ),
+]
+
+
+class ShellImplTest(unittest.TestCase):
+    def _compare_lines(self, first: str, second: str):
+        def normalize(lines: str) -> list[str]:
+            return [x.strip() for x in lines.splitlines() if x.strip()]
+
+        self.assertEqual(normalize(first), normalize(second))
+
+    def test_array_completion(self):
+        self.assertEqual(shell_impl._array_commands(_COMPLETIONS), "foo --bar")
+
+    def test_bash_completion(self):
+        self.assertEqual(
+            shell_impl._bash_commands(_COMPLETIONS, file_completions=[]),
+            "COMPREPLY=( foo --bar )",
+        )
+
+    def test_zsh(self):
+        self._compare_lines(
+            shell_impl._zsh_commands(_COMPLETIONS, file_completions=[], messages=[]),
+            """
+            local -a yabsnap_commands
+            yabsnap_commands=(
+              'foo:foo help'
+            )
+            _describe -t commands 'yabsnap commands' yabsnap_commands
+            local -a yabsnap_options
+            yabsnap_options=(
+              '--bar:bar help'
+            )
+            _describe -t options 'yabsnap options' yabsnap_options
+            """,
+        )
+
+    def test_zsh_only_options(self):
+        completions = [
+            x for x in _COMPLETIONS if x.type == comp_types.CompletionType.OPTION
+        ]
+        self._compare_lines(
+            shell_impl._zsh_commands(
+                completions=completions, file_completions=[], messages=[]
+            ),
+            """
+            local -a yabsnap_options
+            yabsnap_options=(
+              '--bar:bar help'
+            )
+            _describe -t options 'yabsnap options' yabsnap_options
+            """,
+        )
+
+    def test_zsh_no_options(self):
+        completions = [
+            x for x in _COMPLETIONS if x.type != comp_types.CompletionType.OPTION
+        ]
+        self._compare_lines(
+            shell_impl._zsh_commands(
+                completions=completions, file_completions=[], messages=[]
+            ),
+            """
+            local -a yabsnap_commands
+            yabsnap_commands=(
+              'foo:foo help'
+            )
+            _describe -t commands 'yabsnap commands' yabsnap_commands
+            """,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
