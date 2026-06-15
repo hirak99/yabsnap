637b157ea6f350350ef8a931fb96e634ea9f2ec1
Arnab Bose <hirak99@gmail.com>
2026-06-15 15:52:55 +0530

    fix: prevent runtime bytecode generation to avoid package conflicts
    - Set PYTHONDONTWRITEBYTECODE=1 in yabsnap.sh wrapper.
    - Remove -O flag to use standard .pyc files instead of .opt-1.pyc.
    
    This prevents Python from creating unowned bytecode files as root during
    version transitions, which causes "conflicting files" errors during
    package manager updates.

--- src/yabsnap.sh
+++ src/yabsnap.sh
@@ -19,7 +19,8 @@ set -ueo pipefail
 readonly MY_PATH="$(dirname "$(realpath "$0")")"
 
 cd $MY_PATH
+# Do not compile to bytecode, since we create that during installation.
+export PYTHONDONTWRITEBYTECODE=1
 # -u to enable unbuffered, to make sure `yabsnap ... 2>&1 | less` does not
 # change order of logs.
-# -O uses optimized code and ignores asserts. Also creates .opt-1.pyc instead of .pyc.
-python3 -O -u -m code.main "$@"
+python3 -u -m code.main "$@"
